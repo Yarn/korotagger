32b800e4e0bca47c031bdf299648e7eb88317776
Yarn <yarnnd@gmail.com>
2025-04-23 17:34:25 -0700

    remove docker uninitialized postgres check

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,27 +24,10 @@ wait_for_postgres() {
     echo "PostgreSQL is ready and responsive!"
 }
 
-# Function to check if the application is running for the first time
-check_first_time_run() {
-     # this will confirm if "member.required_roles" exist which is the last executed on members.sql
-     check=$(PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -tAc "SELECT 1 FROM information_schema.tables WHERE table_schema = 'member' AND table_name = 'required_roles';")
-     if [ "$check" = "1" ]; then
-         return 1 # Not a First-time run
-     else
-         return 0 # Not a first-time run
-     fi
-}
-
 # Run the wait function
 wait_for_postgres
 
-# Check if it is the first time run
-if check_first_time_run; then
-    echo "First-time run detected. Running SQL initialization scripts..."
-    /app/korotagger migrate
-else
-    echo "Not the first-time run. Skipping SQL initialization."
-fi
+/app/korotagger migrate
 
 # Execute the main application with environment variables
 exec /app/korotagger "$@"
